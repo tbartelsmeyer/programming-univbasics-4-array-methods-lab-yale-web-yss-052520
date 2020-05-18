1f7fa629a83eab3c8835e089a0c53297c0d26cb9
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-18 01:30:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,8 +7,8 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-  reverse_array []
-  reverse_array = array.reverse()
+  reverse_array = []
+  reverse_array = array.reverse
 end
 
 def using_first(array)
